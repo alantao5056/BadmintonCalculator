233295db6277172ddbc4ac11ba220c58d9df5ff4
alantao5056 <alantaolr@outlook.com>
2024-02-05 21:39:34 -0500

    weighted calculations

--- Doubles/src/Main.java
+++ Doubles/src/Main.java
@@ -8,9 +8,19 @@ public class Main {
     Asker asker = new Asker(sc);
     Report report = asker.askRatings();
 
-
+    // simple report
+    System.out.println(report.seperator);
     System.out.println(report.ratingSummary());
-    System.out.println(report.simpleAverageSummary());
+    System.out.println();
+    System.out.println(report.seperator);
+    System.out.println(report.simpleSummary());
+    System.out.println();
+    System.out.println(report.seperator);
+
+    // complex report
+    System.out.println(report.weightedSummary());
+    System.out.println();
+    System.out.println(report.seperator);
 
     sc.close();
   }
--- Doubles/src/Report.java
+++ Doubles/src/Report.java
@@ -1,33 +1,86 @@
 public class Report {
+  // simple
   public double a1;
   public double a2;
   public double b1;
   public double b2;
-  public double saA;
+  public double saA; // simple average
   public double saB;
+  public double wpA; // winning percentage
+  public double wpB;
+  // weighted
+  public double htbdA; // hit to bad difference
+  public double htbdB;
+  public double weightPA; // weighted percentage
+  public double weightPB;
+  public double weightAA; // weighted average
+  public double weightAB;
+  public double weightwpA;
+  public double weightwpB;
+
+
+  public final String seperator = "############################################\n";
+
+  private double round(double x) {
+    return Math.round(x * 100.0) / 100.0;
+  }
+
+  private double roundPercent(double x) {
+    return Math.round(x * 10000.0) / 10000.0;
+  }
 
   public Report(double a1, double a2, double b1, double b2) {
-    this.a1 = a1;
-    this.a2 = a2;
-    this.b1 = b1;
-    this.b2 = b2;
+    this.a1 = round(a1);
+    this.a2 = round(a2);
+    this.b1 = round(b1);
+    this.b2 = round(b2);
   }
 
   public String ratingSummary() {
     return
-        String.format("Team A player 1: %,.1f\nTeam A player 2: %,.1f\nTeam B player 1: %,.1f\nTeam B player 2: %,.1f", a1, a2, b1, b2);
+        String.format("Team A player 1: %,.2f\nTeam A player 2: %,.2f\nTeam B player 1: %,.2f\nTeam B player 2: %,.2f", a1, a2, b1, b2);
   }
 
   private void calcSimpleAverage() {
-    saA = (a1+a2)/2;
-    saB = (b1+b2)/2;
+    saA = round((a1+a2)/2);
+    saB = round((b1+b2)/2);
   }
 
-  public String simpleAverageSummary() {
+  private void calcWinningPercentage() {
+    wpA = saA / (saA + saB);
+    wpB = saB / (saA + saB);
+  }
+
+  public String simpleSummary() {
     calcSimpleAverage();
+    calcWinningPercentage();
     return
         "Simple Average Team A: " + saA +
-        "\nSimple Average Team B: " + saB;
+            "\nSimple Average Team B: " + saB +
+            "\n\nSimple Winning Percentage Team A: " + round(wpA * 100) + "%\nSimple Winning Percentage Team B: " + round(wpB * 100) + "%";
+  }
+
+  private void calcWeighted() {
+    htbdA = Math.max(0, saA-Math.min(b1, b2));
+    htbdB = Math.max(0, saB-Math.min(a1, a2));
+    weightPA = roundPercent(htbdA * 0.05);
+    weightPB = roundPercent(htbdB * 0.05);
+    weightAA = round(saA * (1 + weightPA));
+    weightAB = round(saB * (1 + weightPB));
+    weightwpA = roundPercent(weightAA / (weightAA + weightAB));
+    weightwpB = roundPercent(weightAB / (weightAA + weightAB));
+  }
+
+  public String weightedSummary() {
+    calcWeighted();
+    return
+        "Team A 'Hit to bad' advantage: " + (weightPA*100) +
+        "%\nTeam B 'Hit to bad' advantage: " + (weightPB*100) +
+        "%\n\nTeam A weighted average: " + weightAA +
+        "\nTeam B weighted average: " + weightAB +
+        "\n\nTeam A weighted winning percentage: " + (weightwpA*100) +
+        "%\nTeam B weighted winning percentage: " + (weightwpB*100)+
+        "%";
   }
 
   @Override
